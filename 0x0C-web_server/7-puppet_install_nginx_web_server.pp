c884210b05226d5cceceef36f64a452730f3a3d7
stdjes <133016938+stdjes@users.noreply.github.com>
2024-02-25 03:58:25 -0800

    puppet install

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,20 @@
+# Install Nginx web server (w/ Puppet)
+package { 'nginx':
+  ensure => installed,
+}
+
+file_line { '/redirect_me':
+  ensure => 'present',
+  path   => '/etc/nginx/sites-available/default',
+  after  => 'listen 80 default_server;',
+  line   => 'rewrite ^/redirect_me https://github.com/Y-Baker permanent;',
+}
+
+file { '/var/www/html/index.html':
+  content => 'Hello World!',
+}
+
+service { 'nginx':
+  ensure  => running,
+  require => Package['nginx'],
+}
