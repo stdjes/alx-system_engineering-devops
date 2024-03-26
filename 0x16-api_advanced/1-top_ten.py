278ac5fb712b8399baf3d093ef17c40a90726347
stdjes <133016938+stdjes@users.noreply.github.com>
2024-03-26 14:35:21 -0700

    edits

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,26 +1,36 @@
 #!/usr/bin/python3
-"""Reddit Api Module"""
-
+"""
+Function that queries the Reddit API and prints
+the top ten hot posts of a subreddit
+"""
 import requests
+import sys
 
 
 def top_ten(subreddit):
-    """
-    prints the titles of the first 10 hot posts listed for a given subreddit
-    """
-    if not subreddit:
-        print("None")
-        return
-    url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
+    """ Queries to Reddit API """
+    u_agent = 'Mozilla/5.0'
 
+    headers = {
+        'User-Agent': u_agent
+    }
+
+    params = {
+        'limit': 10
+    }
+
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
     res = requests.get(url,
-                       headers={'User-Agent': 'Edg/118.0.2088.88'},
+                       headers=headers,
+                       params=params,
                        allow_redirects=False)
-
-    if (res.status_code / 100) >= 4:
-        print('None')
+    if res.status_code != 200:
+        print(None)
         return
-
-    posts = res.json().get('data').get('children')
-    for post in posts:
-        print(post.get('data').get('title'))
+    dic = res.json()
+    hot_posts = dic['data']['children']
+    if len(hot_posts) is 0:
+        print(None)
+    else:
+        for post in hot_posts:
+            print(post['data']['title'])
